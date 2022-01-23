5704d8ef4aa6977976584bef6d55a1b11befed13
오채환 <coghks56@gmail.com>
2022-01-23 19:39:03 +0900

    Remove unnecessary file

--- kakao_map_crawler/kakao_restaurant_review_crawler.py
+++
@@ -1,9 +0,0 @@
-from util.chromedriver import SetupBrowserEnvironment
-from util.extractor import extract_review_info
-
-
-def extract_restaurant_review(restaurant_id):
-    with SetupBrowserEnvironment() as browser:
-        restaurant_id = '1316757553' # 이원화구포국시 카카오맵ID
-        reviews = extract_review_info(browser, restaurant_id=restaurant_id)
-        print('reviews', reviews)
