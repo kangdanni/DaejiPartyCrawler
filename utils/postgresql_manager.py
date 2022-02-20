71f1dfd915bbb240b3c77f071e2cc0f342a71a55
강단 <kkangdanni@sk.com>
2022-02-20 20:59:54 +0900

    insert_many --> for문으로 변경

--- utils/postgresql_manager.py
+++ utils/postgresql_manager.py
@@ -12,12 +12,15 @@ load_dotenv(dotenv_path)
 class PostgresqlManager:
     def __init__(self):
         self.connection = psycopg2.connect(
-            host=getenv('DB_HOST'), 
-            dbname=getenv('DB_NAME'), 
+            host=getenv('DB_HOST'),
+            dbname=getenv('DB_NAME'),
             user=getenv('DB_USERNAME'),
-            password=getenv('DB_PASSWORD'), 
+            password=getenv('DB_PASSWORD'),
             port=getenv('DB_PORT'))
+        #에러시 transaction 오류로 인해 autocommit으로 변경
+        self.connection.autocommit = True
         self.cursor = self.connection.cursor()
+
         print(f"connnection established to {getenv('DB_HOST')}")
 
     def find(self, table_name, columns, conditions={}):
@@ -33,19 +36,20 @@ class PostgresqlManager:
 
     def insert_one(self, table_name, data):
         keys = str(tuple(data.keys())).replace('\'', '')
-        values = tuple(data.values()) 
+        values = tuple(data.values())
         self.cursor.execute(f"INSERT INTO {table_name} {keys} VALUES {values}")
         self.connection.commit()
 
     def insert_many(self, table_name, data):
         if len(data) == 0:
             raise ValueError('`data` can not be an empty list.')
-        keys = str(tuple(data[0].keys())).replace('\'', '')
-        placeholders = ', '.join(['%s'] * len(data[0].keys()))
-        query = f"INSERT INTO {table_name} {keys} VALUES ({placeholders})"
-        values = [tuple(item.values()) for item in data]
-        self.cursor.executemany(query, values)
-        self.connection.commit()
+
+        for i in data :
+            try :
+                PostgresqlManager.insert_one(self, table_name, i)
+            except Exception as e:
+                print("Error with insert Review ",i)
+                print("Error trace:", e)
 
     def __del__(self):
         self.connection.close()
