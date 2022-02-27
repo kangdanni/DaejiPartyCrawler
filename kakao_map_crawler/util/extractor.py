ccd523f97e99b5e72b5bf4735687f26c1f63dbfb
오채환 <coghks56@gmail.com>
2022-02-27 20:02:22 +0900

    Update review crawler to scrape multiple pages

--- kakao_map_crawler/util/extractor.py
+++ kakao_map_crawler/util/extractor.py
@@ -1,16 +1,15 @@
+from time import sleep
+
 from kakao_map_crawler.util.logger import KakaoMapCrawlingLogger
 from kakao_map_crawler.util.util import web_adress_navigator
 from kakao_map_crawler.util.exceptions import PageNotFound404, NoRestaurantPageFound
 
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
 
-def extract_review_info(browser, restaurant_id):
-    try:
-        restaurant_link = f"https://place.map.kakao.com/{restaurant_id}"
-        web_adress_navigator(browser, restaurant_link)
-        KakaoMapCrawlingLogger.logger().info(f"Extracting information from {restaurant_id}")
-    except PageNotFound404 as e:
-        raise NoRestaurantPageFound(e)
-    
+
+def extract_review_info(browser):
     main_article_section = browser.find_element_by_css_selector("#kakaoContent > #mArticle") 
     comment_section = main_article_section.find_element_by_css_selector("div[data-viewid='comment']")
     review_lists = comment_section.find_elements_by_css_selector(".list_evaluation > li")
@@ -26,4 +25,48 @@ def extract_review_info(browser, restaurant_id):
                 'rating': int(rating_element.text),
                 'comment': comment_element.text,
             })
+    return reviews
+
+
+def extract_reviews(browser, restaurant_id):
+    try:
+        restaurant_link = f"https://place.map.kakao.com/{restaurant_id}"
+        web_adress_navigator(browser, restaurant_link)
+        KakaoMapCrawlingLogger.logger().info(f"Extracting information from {restaurant_id}")
+    except PageNotFound404 as e:
+        raise NoRestaurantPageFound(e)
+
+    reviews = []
+    reviews.extend(extract_review_info(browser))
+
+    try:
+        page_count = len(browser.find_elements_by_class_name('link_page'))
+        index = 3
+        for i in range(page_count - 1):
+            browser.find_element_by_css_selector('#mArticle > div.cont_evaluation > div.evaluation_review > div > a:nth-child(' + str(index) +')').send_keys(Keys.ENTER)
+            sleep(1)
+            reviews.extend(extract_review_info(browser))
+            index += 1
+        browser.find_element_by_link_text('다음').send_keys(Keys.ENTER) # 5페이지가 넘는 경우 다음 버튼 누르기
+        sleep(1)
+        reviews.extend(extract_review_info(browser))
+    except (NoSuchElementException, ElementNotInteractableException):
+        print("no review in crawling")
+
+    # 그 이후 페이지
+    while True:
+        index = 4
+        try:
+            page_num = len(browser.find_elements_by_class_name('link_page'))
+            for i in range(page_num-1):
+                browser.find_element_by_css_selector('#mArticle > div.cont_evaluation > div.evaluation_review > div > a:nth-child(' + str(index) +')').send_keys(Keys.ENTER)
+                sleep(1)
+                reviews.extend(extract_review_info(browser))
+                index += 1
+            browser.find_element_by_link_text('다음').send_keys(Keys.ENTER) # 10페이지 이상으로 넘어가기 위한 다음 버튼 클릭
+            sleep(1)
+            reviews.extend(extract_review_info(browser))
+        except (NoSuchElementException, ElementNotInteractableException):
+            print("no review in crawling")
+            break
     return [{**review, 'restaurant_id': int(restaurant_id)} for review in reviews]
--- restaurant_info_scraper.py
+++ restaurant_info_scraper.py
@@ -1,22 +1,14 @@
-import psycopg2
-from pprint import pprint
-from dotenv import load_dotenv
-from os.path import abspath, dirname, join
-from os import getenv
-
 from instagram_crawler.crawl_profile_with_login import extract_choizaroad_location_names
 from kakao_local_api.kakao_restaurant_info_scraper import get_place_info
 from kakao_map_crawler.util.chromedriver import SetupBrowserEnvironment
-from kakao_map_crawler.util.extractor import extract_review_info
+from kakao_map_crawler.util.extractor import extract_reviews
 from utils.postgresql_manager import PostgresqlManager
-
-
-postgresql_manager = PostgresqlManager()
 
 
 with SetupBrowserEnvironment() as browser:
     location_names = extract_choizaroad_location_names(browser)
-    query_result = PostgresqlManager().find(table_name='place', columns=['place_name'])
+    postgresql_manager = PostgresqlManager()
+    query_result = postgresql_manager.find(table_name='place', columns=['place_name'])
     existing_restaurants = [item[0] for item in query_result] if len(query_result) > 0 else []
     location_names = [location_name for location_name in location_names \
                         if location_name not in existing_restaurants]
@@ -31,10 +23,10 @@ with SetupBrowserEnvironment() as browser:
             print("Error trace:", e)
 
         try:
-            reviews = extract_review_info(browser, restaurant_id=place_info['id'])
+            reviews = extract_reviews(browser, restaurant_id=place_info['id'])
             postgresql_manager.insert_many(table_name='review', data=reviews)
             print(f'{place_info["place_name"]} review data inserted!')
         except Exception as e:
             print("[Review] Error with location " + location_name)
             print("Error trace:", e)
-        
+    
